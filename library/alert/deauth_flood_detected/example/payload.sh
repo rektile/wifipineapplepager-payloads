8d6d9ee5f98e34607a31dce7248ebe02670e7739
Darren Kitchen <darren@hak5.org>
2025-12-18 14:31:13 -0600

    Update payload.sh

--- library/alert/deauth_flood_detected/example/payload.sh
+++ library/alert/deauth_flood_detected/example/payload.sh
@@ -7,9 +7,9 @@
 ALERT "$_ALERT_DENIAL_MESSAGE"
 
 # Additional variables include:
-# $_ALERT										              "deauth_flood_detected"
-# $_ALERT_DENIAL_MESSAGE						      human readable alert content "deauth/disassoc flood from ..."
-# $_ALERT_DENIAL_SOURCE_MAC_ADDRESS		  	source mac
-# $_ALERT_DENIAL_DESTINATION_MAC_ADDRESS	destination mac
-# $_ALERT_DENIAL_AP_MAC_ADDRESS				    ap/bssid mac address regardless of direction
-# $_ALERT_DENIAL_CLIENT_MAC_ADDRESS			  client mac address regardless of direction
+# $_ALERT                                 "deauth_flood_detected"
+# $_ALERT_DENIAL_MESSAGE                  human readable alert content "deauth/disassoc flood from ..."
+# $_ALERT_DENIAL_SOURCE_MAC_ADDRESS       source mac
+# $_ALERT_DENIAL_DESTINATION_MAC_ADDRESS  destination mac
+# $_ALERT_DENIAL_AP_MAC_ADDRESS           ap/bssid mac address regardless of direction
+# $_ALERT_DENIAL_CLIENT_MAC_ADDRESS       client mac address regardless of direction
